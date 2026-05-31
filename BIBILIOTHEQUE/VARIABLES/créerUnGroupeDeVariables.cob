5f9f3153de7b176c3ff1ea77b2c24c660b3d0b84
Thomas PREVOST <tprevost802@gmail.com>
2026-05-31 08:29:35 +0200

    groupe de variables

---
+++ BIBILIOTHEQUE/VARIABLES/créerUnGroupeDeVariables.cob
@@ -0,0 +1,14 @@
+        IDENTIFICATION DIVISION.
+            PROGRAM-ID. "GROUPEDEVARIABLES".
+        ENVIRONMENT DIVISION.
+        DATA DIVISION.
+            WORKING-STORAGE SECTION.
+            01 DATEHEURE-01.
+                02 DATE-01    PIC   9(8).
+                02 HEURE-01   PIC   9(4). 
+        PROCEDURE DIVISION.
+            DISPLAY DATE-01.
+            MOVE 31052026 TO DATE-01.
+            DISPLAY DATE-01.
+            DISPLAY HEURE-01.
+            STOP RUN.
